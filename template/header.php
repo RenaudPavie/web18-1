d4708b47caba3a22fc77fce6eee20a23af9557c9
Renaud <renaud.hpavie@gmail.com>
2019-05-06 16:11:23 +0200

    modif param url

--- template/header.php
+++ template/header.php
@@ -3,7 +3,7 @@
     <ul>
       <li><a href="index.php?page=home" title="Page d'accueil">Accueil</a></li>
       <li><a href="index.php?page=contact" title="Page d'articles">Contact</a></li>
-      <li><a href="index.php?page=article" title="Page d'articles">article</a></li>
+      <li><a href="index.php?page=news" title="Page d'articles">article</a></li>
       <li><a href="index.php?page=inscription" title="Page d'articles">Inscription</a></li>
       <li><a href="index.php?page=connexion" title="Page d'articles">Connexion</a></li>
       <li><a href="index.php?page=compte" title="Page d'articles">Mon compte</a></li>
